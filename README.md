bfe39909fe661ad72d357d9bb6e49cf1d557a872
kalyan485 <kalyanc1242@gmail.com>
2019-05-02 11:13:41 +0530

    Memorygame current code

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Memory-Game
+# Memory Game Project:
+======================
+The main objective of this game to check our memory by selecting pairs of cards with pair of images. In this game we going to learn DOM manipulations using JavaScript including customization of styles and also focusing on JavaScript arrow functions.
+
+
+## Table of Contents
+
+1.Download the file from [GitHUB]  (https://github.com/udacity/fend-project-memory-game).
+2.Extract the file.
+3.The file Contains :
+  1.css folder, //Modify or Use for styling purpose.
+  2.img folder, //Image files if you needed.
+  3.js folder,  //Based on instructions follow and make the requirements.
+4.index.html  //Sample example html file given ande it to modify the page.
+5. README.md and other // Share your information in README.md file.
+
+
+* [Instructions](#instructions)
+
+
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+To get started, open `js/app.js` and start building out the app's functionality.
+
+I completed this project by did the following procedure.
+ + Clicked the card.
+ + Kept revealing cards and working your memory to remember each unveiled card.
+ + Matched cards properly with less moves and in faster time.
+ + Created a cards to hold all cards that shuffles when game is refreshed.
+ + Created a counter to count the number of moves made by player and timer to know the duration of a play.
+ + Added effects to cards when they match and are unmatched.
+ + Displayed moves,Stars rating and time to completed game by using sweetalert.js.
+
+
+For specific, detailed instructions,look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+## Contributing
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+* [Contributing](#contributing)
---
+++ js/app.js
@@ -0,0 +1,176 @@
+/*
+Create a list that holds all of your cards
+*/
+let card = document.getElementsByClassName("card");
+let cardsList = [...card];
+let moves = 0;
+let counter = document.querySelector(".moves");
+var time = 0;
+let timer = document.querySelector(".timer");
+let match = 0;
+let star = document.querySelectorAll(".fa-star");
+let stars = 3;
+//console.log(star);
+window.onload = gameStart();
+clickedCards = [];
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+// Shuffle function from http://stackoverflow.com/a/2450976
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+// cards array holds all cards
+// cards array holds all cards
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+// loop to add event listeners to each card
+for (var i = 0; i < cardsList.length; i++) {
+  var displayCard = function() {
+    this.classList.add("open", "show", "disabled");
+    console.log(this);
+  }
+  cardsList[i].addEventListener("click", displayCard);
+  cardsList[i].addEventListener("click", openCard);
+};
+/*update cards*/
+function gameStart() {
+  var cards = document.getElementById("deck");
+  var shuffledCards = shuffle(cardsList);
+  for (var i = 0; i < shuffledCards.length; i++) {
+    [].forEach.call(shuffledCards, function(item) {
+      // console.log(item);
+      cards.appendChild(item);
+    });
+  }
+}
+/*open card functionality*/
+function openCard() {
+  time = time + 1;
+  clickedCards.push(this);
+  var len = clickedCards.length;
+  if (len === 2) {
+    moveCounter();
+
+    /*cards matching if their class name is equal */
+    if (clickedCards[0].children[0].classList.item(1) === clickedCards[1].children[0].classList.item(1)) {
+      match += 1;
+      clickedCards[0].classList.add("match");
+      clickedCards[1].classList.add("match");
+      clickedCards[0].classList.remove("open", "show");
+      clickedCards[1].classList.remove("open", "show");
+      clickedCards = [];
+
+      /*After completion of game a sweet alert with rating, moves and time to complete game*/
+      if (match === 8) {
+        clearInterval(interval);
+
+        var span = document.createElement("span");
+        span.inn
+        erHTML = " Star rating: " + (stars + "/3") + "<br>" + " Moves: " + moves + "<br>" + " Time to complete game: " + timer.innerHTML;
+        swal({
+
+          title: "Good job!",
+          content: span,
+          text: "Successfully completed the game",
+          icon: "success",
+          button: "Play again"
+
+        }).then(
+          isConfirm => {
+            if (isConfirm) {
+              location.reload();
+            }
+          });
+
+      }
+    }
+    /*If cards not matching setTimeOut */
+    else {
+      clickedCards[0].classList.add("unmatched");
+      clickedCards[1].classList.add("unmatched");
+      setTimeout(function() {
+        clickedCards[0].classList.remove("show", "open", "disabled", "unmatched");
+        clickedCards[1].classList.remove("show", "open", "disabled", "unmatched");
+        clickedCards = [];
+      }, 300);
+    }
+  }
+  /*when time == 1 call the startTimer function*/
+  if (time == 1) {
+    startTimer();
+  }
+}
+
+/*moves count and stars display respective moves count*/
+
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+  if(moves<=10){
+    stars=3;
+}
+
+  else if (moves > 8 && moves < 16) {
+    stars = 2;
+    star[2].style.display = "none";
+    //star.innerHTML=document.getElementsByClassName(star[stars]);
+  } else if (moves >= 17 && moves <= 20) {
+    stars = 1;
+    star[1].style.display = "none";
+  }
+}
+
+
+/*time start when game begins*/
+
+
+var seconds = 0,
+  minutes = 0,
+  hour = 0;
+
+var interval;
+
+function startTimer() {
+  interval = setInterval(function() {
+    timer.innerHTML = minutes + "mins " + seconds + "secs";
+    seconds++;
+    if (seconds == 60) {
+      minutes++;
+      seconds = 0;
+    }
+    if (minutes == 60) {
+      hour++;
+      minutes = 0;
+    }
+  }, 1000);
+}
+
+/*reset game when click on refresh button*/
+
+function resetGame() {
+  window.location.reload();
+}
